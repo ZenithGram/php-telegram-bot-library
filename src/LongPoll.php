30b350c7bbd1d05868d688e0173b2c2f08462dec
Zhenya <em167116@gmail.com>
2025-12-21 20:54:51 +0300

    refactor(LongPoll): улучшение структуры и документации - Удалено свойство `$isRunning` и заменено локальной переменной в `listen()`. - Добавлены PHPDoc-блоки для методов `create()`, `skipOldUpdates()` и `listen()`. - Методы, связанные с обработкой ошибок, перегруппированы для лучшей читаемости.

--- src/LongPoll.php
+++ src/LongPoll.php
@@ -16,7 +16,6 @@ class LongPoll
     private int $timeout;
     private int $offset = 0;
     private bool $skipOldUpdates = false;
-    private bool $isRunning = false;
     private bool $shortTrace = false;
     private string $pathFiler = '';
     private array|null $debug_chat_ids = null;
@@ -29,11 +28,26 @@ class LongPoll
         $this->timeout = $timeout;
     }
 
+    /**
+     * Создает объект класса LongPoll
+     *
+     * @param string $token Токен Telegram
+     * @param int $timeout Сколько будет удерживаться соединение с Telegram
+     *
+     * @return LongPoll
+     * @see https://zenithgram.github.io/classes/longpoll#create
+     */
     public static function create(string $token, int $timeout = 20): self
     {
         return new self(new ApiClient($token), $timeout);
     }
 
+    /**
+     * Пропускает старые обновления
+     *
+     * @return LongPoll
+     * @see https://zenithgram.github.io/classes/longpoll#skipoldupdates
+     */
     public function skipOldUpdates(): self
     {
         $this->skipOldUpdates = true;
@@ -42,97 +56,22 @@ class LongPoll
     }
 
     /**
-     * Активирует дебаг режим
+     * Запускает опрос серверов Telegram
      *
-     * Чтобы он работал, нужно задать обработчик, либо перечислить id, куда
-     * будет отправлены ошибки
+     * @param Closure $handler
      *
-     * @return LongPoll
-     *
-     * @see https://zenithgram.github.io/classes/errorhandler#enableDebug
+     * @return void
+     * @see https://zenithgram.github.io/classes/longpoll#listen
      */
-    public function enableDebug(): self
-    {
-        $this->debug = true;
-
-        return $this;
-    }
-
-    /**
-     * Устанавливает ID, куда будут отправлены возникшие ошибки
-     *
-     * @param int|string|array $ids ID пользователя или чата
-     *
-     * @return LongPoll
-     *
-     * @see https://zenithgram.github.io/classes/errorhandler#setSendIds
-     */
-    public function setSendIds(int|string|array $ids): self
-    {
-        $this->debug_chat_ids = is_array($ids) ? $ids : [$ids];
-
-        return $this;
-    }
-
-    /**
-     * Устанавливает обработчик, который срабатывает при возникновении ошибки
-     *
-     * @param callable $handler Обработчик. Пример: function (ZG $tg, Throwable
-     *                          $e)
-     *
-     * @return LongPoll
-     *
-     * @see https://zenithgram.github.io/classes/errorhandler#setHandler
-     */
-    public function setHandler(callable $handler): self
-    {
-        $this->handler = $handler(...);
-
-        return $this;
-    }
-
-    /**
-     * Отображать полный трейт или сокращенный
-     *
-     * @param bool $short
-     *
-     * @return LongPoll
-     *
-     * @see https://zenithgram.github.io/classes/errorhandler#shortTrace
-     */
-    public function shortTrace(bool $short = true): self
-    {
-        $this->shortTrace = $short;
-
-        return $this;
-    }
-
-    /**
-     * Устанавливает фильтр на путь к файлу, чтобы не отображать его
-     *
-     * @param string $filter "/path/to/your/project"
-     *
-     * @return LongPoll
-     *
-     * @see https://zenithgram.github.io/classes/errorhandler#setTracePathFilter
-     */
-    public function setTracePathFilter(string $filter): self
-    {
-        $this->pathFiler = $filter;
-
-        return $this;
-    }
-
-
     public function listen(Closure $handler): void
     {
-        $this->isRunning = true;
+        $isRunning = true;
 
         if ($this->skipOldUpdates) {
             $this->dropPendingUpdates();
         }
 
-        while ($this->isRunning) {
+        while ($isRunning) {
             try {
                 $updates = $this->fetchUpdates();
 
@@ -217,5 +156,87 @@ class LongPoll
         } catch (Throwable $e) {
             $tgInstance->reportException($e);
         }
+    }
+
+    /**
+     * Активирует дебаг режим
+     *
+     * Чтобы он работал, нужно задать обработчик, либо перечислить id, куда
+     * будет отправлены ошибки
+     *
+     * @return LongPoll
+     *
+     * @see https://zenithgram.github.io/classes/errorhandler#enableDebug
+     */
+    public function enableDebug(): self
+    {
+        $this->debug = true;
+
+        return $this;
+    }
+
+    /**
+     * Устанавливает ID, куда будут отправлены возникшие ошибки
+     *
+     * @param int|string|array $ids ID пользователя или чата
+     *
+     * @return LongPoll
+     *
+     * @see https://zenithgram.github.io/classes/errorhandler#setSendIds
+     */
+    public function setSendIds(int|string|array $ids): self
+    {
+        $this->debug_chat_ids = is_array($ids) ? $ids : [$ids];
+
+        return $this;
+    }
+
+    /**
+     * Устанавливает обработчик, который срабатывает при возникновении ошибки
+     *
+     * @param callable $handler Обработчик. Пример: function (ZG $tg, Throwable
+     *                          $e)
+     *
+     * @return LongPoll
+     *
+     * @see https://zenithgram.github.io/classes/errorhandler#setHandler
+     */
+    public function setHandler(callable $handler): self
+    {
+        $this->handler = $handler(...);
+
+        return $this;
+    }
+
+    /**
+     * Отображать полный трейт или сокращенный
+     *
+     * @param bool $short
+     *
+     * @return LongPoll
+     *
+     * @see https://zenithgram.github.io/classes/errorhandler#shortTrace
+     */
+    public function shortTrace(bool $short = true): self
+    {
+        $this->shortTrace = $short;
+
+        return $this;
+    }
+
+    /**
+     * Устанавливает фильтр на путь к файлу, чтобы не отображать его
+     *
+     * @param string $filter "/path/to/your/project"
+     *
+     * @return LongPoll
+     *
+     * @see https://zenithgram.github.io/classes/errorhandler#setTracePathFilter
+     */
+    public function setTracePathFilter(string $filter): self
+    {
+        $this->pathFiler = $filter;
+
+        return $this;
     }
 }
